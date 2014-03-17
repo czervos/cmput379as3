e9e68268676b6e3676162c1776aa691fc427bcc8
czervos <czervos@ualberta.ca>
2014-03-17 15:04:42 -0600

    Added directives for ammo, lanes, and object shapes

--- saucer.c
+++ saucer.c
@@ -2,6 +2,13 @@
  * Costa Zervos 
  */
 
+#include <stdio.h>
+
+#define AMMO 10 /* Total number of rockets */
+#define LANES 3 /* Top lines enemy saucers can occupy */
+#define SAUCER "<--->" /* Enemy saucer shape */
+#define LAUNCHER "|" /* User launcher shape */
+
 int main(int argc, char *argv[])
 {
         return 0;
