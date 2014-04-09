a002ff5bf6f5e4ebf3a349a44187b8f1e045c88e
czervos <czervos@ualberta.ca>
2014-04-08 22:18:42 -0600

    Implemented ammo drops AND fixed bug where destroyed saucers would remain behind

--- saucer.c
+++ saucer.c
@@ -16,13 +16,41 @@ Saucer threads - max 10?
 
 These threads will animate the props and make Rockets and Saucers disappear if they hit the end of the screen
 
-STRIKE THREAD
-Will loop through all the rocket structs and compare the row/col with the row/col of each saucer; if they match
-a strike has occured and will set a flag in both. The animate thread will detect that the flag has been set
-and make them disappear.
+in strike_check
+
+	when a strike is successfully detected:
+		1. on a random chance, iterate through
+		   the ammo_drop_array and look for the first
+		   live=0 ammo drop
+		2. set that ammo drop to the coordinates of where the strike
+		   happend, set it to live=1, and set it to new=1
+		3. in some other method that is continually running a for loop
+ 		   that checks to see if any of the rockets are live=1 and new=1
+			a. if it finds one, set that new=0 and create an animate
+			   ammo_drop thread for it.
+                4. have another method continually running called 
+                   ammo_collection check which will check to see if its coordinates
+		   match the coordinates of the launcher: if it does then 
+		   it is a sucessful collection and the user's ammo count should
+		   go up.
+		5. animate ammo drop will also check to see if the drop passed
+	           the launcher. if it has, set the ammo_drop.live = 0 and kill
+		   that animate thread.
+		6. Out of ammo fail condition needs to make sure no ammo drops
+		   are in the proccess of falling instead of checking to see if
+		   any rockets are flying.
+		7. Also remove rocket strike ammo increment.
 
 Bottom right corner of terminal (LINES-1 COLS-1)
 Top left corner of terminal (0 0)
+
+0x20 space character - memset to dynamically make blank
+
+BUG FOR THINGS STICKING AROUND:
+The issue is that i create a new thread when something is considered dead. So during the
+animation function for say a saucer, it detects its dead and starts exiting, such as priting the explosion
+and then clearing it out. But during this time, the thread can potentially get cancelled by having a new
+thread created in its place, resulting in a saucer or explosion staying behind.
 
 */
 
@@ -40,11 +68,13 @@ Top left corner of terminal (0 0)
 #define SAUCER "<--->" /* Enemy saucer shape */
 #define ROCKET "^" /* Rocket shape */
 #define EXPLOSION "~%@%~" /* Explosion shape */
+#define AMMO_DROP "#" /* Ammo drop shape */
 #define SAUCER_SCORE 100  /* Points for killing a saucer */
 #define SAUCER_AMMO 2 /* Ammo for killing a saucer */
 #define MAX_PLAYERS 1 /* Max number of players that can play */
 #define MAX_ROCKETS 30 /* Max number of rockets on screen */
 #define MAX_SAUCERS 10 /* Max number of saucers on screen */
+#define MAX_AMMO_DROPS 10 /* Max number of ammo drops on screen */
 #define ESCAPE_NUM 10 /* Number of escapes before game is over */
 #define TIME 60 /* Gameplay time in seconds */
 #define TUNIT 20000 /* Time unit in microseconds */
@@ -70,11 +100,22 @@ struct saucer {
         int col; /* Column location */
         int delay; /* Delay time unit for animation */
         int live; /* Flag indicating whether or not saucer is active */
+        int thread; /* Flag indicating whether or not this struct is being used by a thread */
+};
+
+struct ammo_drop {
+        char *str; /* Look of ammo drop */
+        int row; /* Row location */
+        int col; /* Column location */
+        int live; /* Flag indicating whether or not ammo drop is active */
+        int new; /* Binary value to determine whether ammo drop is newly created */
+        int thread; /* Flag indicating whether or not this struct is being used by a thread */
 };
 
 pthread_mutex_t MX = PTHREAD_MUTEX_INITIALIZER; /* Mutex lock */
 int QUIT_FLAG = 0; /* Inidicates whether game should be quit */
 int LAUNCH_FLAG = 0; /* Indicates whether a rocket should be launched */
+int AMMO_FLAG = 0; /* Indicates whether ammo should be dropped */
 int P1AMMO = AMMO; /* Player 1's ammo count */ // TODO put in own struct - requires changing stuff control_input function
 int P1SCORE = 0; /* Player 1's score */ // TODO put in own struct
 int ESCAPE = 0; /* Number of escaped saucers */
@@ -84,12 +125,16 @@ void setup_curses();
 void setup_players(struct launcher[]);
 void setup_rockets(struct rocket[]);
 void setup_saucers(struct saucer[]);
+void setup_ammo_drops(struct ammo_drop[]);
 void *animate_launcher(void *);
 void *animate_rocket(void *);
 void *animate_saucer(void *);
+void *animate_ammo_drops(void *);
 void *saucer_factory(void *);
 void *control_input(void *);
-void strike_check(struct rocket[], struct saucer[]);
+void *ammo_drop_factory(void *);
+void strike_check(struct rocket[], struct saucer[], struct ammo_drop[]);
+void ammo_collection_check(struct ammo_drop[], struct launcher);
 void *HUD_display();
 void *countdown_timer();
 void splash_screen();
@@ -105,9 +150,11 @@ int main(int argc, char *argv[])
         struct launcher launcher_props[MAX_PLAYERS]; /* Player props */
         struct rocket rocket_props[MAX_ROCKETS];
         struct saucer saucer_props[MAX_SAUCERS];
+        struct ammo_drop ammo_drop_props[MAX_AMMO_DROPS];
         pthread_t launcher_threads[MAX_PLAYERS];
         pthread_t rocket_threads[MAX_ROCKETS];
         pthread_t saucer_factory_thread;
+        pthread_t ammo_drop_factory_thread;
         pthread_t control_thread;
         pthread_t HUD_thread;
         pthread_t timer_thread;
@@ -118,10 +165,12 @@ int main(int argc, char *argv[])
         /* Creates random seed based on pid */
         srand(getpid());
 
+        /* Sets ups curses and the various structure arrays */
         setup_curses();
         setup_players(launcher_props);
         setup_rockets(rocket_props);
         setup_saucers(saucer_props);
+        setup_ammo_drops(ammo_drop_props);
 
         splash_screen();
 
@@ -140,6 +189,7 @@ int main(int argc, char *argv[])
         pthread_create(&saucer_factory_thread, NULL, saucer_factory, &saucer_props);
         pthread_create(&HUD_thread, NULL, HUD_display, NULL);
         pthread_create(&timer_thread, NULL, countdown_timer, NULL);
+        pthread_create(&ammo_drop_factory_thread, NULL, ammo_drop_factory, &ammo_drop_props); // TODO commenting this out prevents crash
 
         /* Game loop */
         while (!QUIT_FLAG) {
@@ -157,14 +207,17 @@ int main(int argc, char *argv[])
                 pthread_create(&rocket_threads[i], NULL, animate_rocket, &rocket_props[i]); // TODO error case
             }
             /* Checks if any live rockets hit a saucer */
-            strike_check(rocket_props, saucer_props);
+            strike_check(rocket_props, saucer_props, ammo_drop_props);
+
+            /* Checks if any ammo drop was collected */
+            ammo_collection_check(ammo_drop_props, launcher_props[0]);
             /* Checks escape endgame condition */
             if (ESCAPE == ESCAPE_NUM)
                     QUIT_FLAG = 1;
             /* Checks if ammo depleted and there are no live rockets for endgame condition */
             if (P1AMMO == 0) {
-                for (i=0; i < MAX_ROCKETS; i++) {
-                    if (rocket_props[i].live == 1)
+                for (i=0; i < MAX_AMMO_DROPS; i++) {
+                    if (ammo_drop_props[i].live == 1)
                             break;
                     if (i == MAX_ROCKETS-1)
                             QUIT_FLAG = 1;
@@ -187,7 +240,7 @@ int main(int argc, char *argv[])
                 escape_fail_screen();
         else if (P1AMMO == 0)
                 no_ammo_fail_screen();
-// TODO must close threads when done
+// TODO must close threads when done -- I think at this point most threads close themselves - double check
         endwin();
         return 0;
 }
@@ -261,6 +314,24 @@ void setup_saucers(struct saucer saucer_array[])
             saucer_array[i].col = 0;
             saucer_array[i].delay = 0;
             saucer_array[i].live = 0;
+            saucer_array[i].thread = 0;
+        }
+}
+
+/*
+ * TODO add description for this function
+ * Setup ammo drops
+ */
+void setup_ammo_drops(struct ammo_drop ammo_drop_array[])
+{
+        int i;
+        for (i=0; i < MAX_AMMO_DROPS; i++) {
+            ammo_drop_array[i].str = AMMO_DROP;
+            ammo_drop_array[i].row = 0;
+            ammo_drop_array[i].col = 0;
+            ammo_drop_array[i].live = 0;
+            ammo_drop_array[i].new = 0;
+            ammo_drop_array[i].thread = 0;
         }
 }
 
@@ -421,7 +492,7 @@ void *animate_saucer(void *arg)
         }
 
         /* Remove saucer*/
-        usleep(TUNIT * 5);
+        usleep(TUNIT * 5); // TODO why is this *5? To show explosion
         pthread_mutex_lock(&MX);
         move(mysaucer->row, mysaucer->col-1); /* Go to saucer's last position */
         addstr(blank); /* Clear the saucer */
@@ -434,8 +505,102 @@ void *animate_saucer(void *arg)
         mysaucer->row = 0;
         mysaucer->col = 0;
         mysaucer->delay = 0;
+        mysaucer->thread = 0;
         pthread_exit(NULL);
 }
+
+/*
+ * TODO add description for this function
+ * Animate the ammo drops
+ */
+void *animate_ammo_drops(void *arg)
+{
+        struct ammo_drop *myammo = arg;
+
+        /* Displays initial ammo drop */
+        usleep(TUNIT);
+        pthread_mutex_lock(&MX);
+        move(myammo->row, myammo->col);
+        addstr(myammo->str);
+        myammo->row += 1; /* Decrements ammo drop position */
+        move(LINES-1, COLS-1);
+        refresh();
+        pthread_mutex_unlock(&MX);
+
+        /* Animates rocket upward while rocket position is not 0 and rocket is live*/
+        while((myammo->row <= LINES-1) && myammo->live == 1) {
+            usleep(TUNIT * 10); /* Speed at which the ammo drop will fall */
+            pthread_mutex_lock(&MX);
+            move(myammo->row-1, myammo->col); /* Moves to old instance of ammo drop */
+            addch(' '); /* Removes it */
+            move(myammo->row, myammo->col); /* Moves to new ammo drop location */
+            addstr(myammo->str); /* Prints it */
+            myammo->row += 1; /* Increments ammo drop position */
+            move(LINES-1, COLS-1); /* Park cursor */
+            refresh();
+            pthread_mutex_unlock(&MX);
+        }
+
+        /* Remove last visible instance of rocket */
+        usleep(TUNIT);
+        pthread_mutex_lock(&MX);
+        move(myammo->row-1, myammo->col);
+        addch(' ');
+        move(LINES-1, COLS-1);
+        refresh();
+        pthread_mutex_unlock(&MX);
+
+        /* Cleanup before exiting thread */
+        myammo->live = 0;
+        myammo->row = 0;
+        myammo->col = 0;
+        myammo->thread = 0;
+        pthread_exit(NULL); // TODO difference between this and pthread_cancel in terms of threads array?
+}
+/* TODO REMOVE BACKUP */
+/* void *animate_ammo_drops(void *arg) */
+/* { */
+/*         struct ammo_drop *myammo = arg; */
+
+/*         /\* Displays initial ammo drop *\/ */
+/*         usleep(TUNIT); */
+/*         pthread_mutex_lock(&MX); */
+/*         move(myammo->row, myammo->col); */
+/*         addstr(myammo->str); */
+/*         myammo->row += 1; /\* Decrements ammo drop position *\/ */
+/*         move(LINES-1, COLS-1); */
+/*         refresh(); */
+/*         pthread_mutex_unlock(&MX); */
+
+/*         /\* Animates rocket upward while rocket position is not 0 and rocket is live*\/ */
+/*         while((myammo->row <= LINES-1) && myammo->live == 1) { */
+/*             usleep(TUNIT * 10); /\* Speed at which the ammo drop will fall *\/ */
+/*             pthread_mutex_lock(&MX); */
+/*             move(myammo->row-1, myammo->col); /\* Moves to old instance of ammo drop *\/ */
+/*             addch(' '); /\* Removes it *\/ */
+/*             move(myammo->row, myammo->col); /\* Moves to new ammo drop location *\/ */
+/*             addstr(myammo->str); /\* Prints it *\/ */
+/*             myammo->row += 1; /\* Increments ammo drop position *\/ */
+/*             move(LINES-1, COLS-1); /\* Park cursor *\/ */
+/*             refresh(); */
+/*             pthread_mutex_unlock(&MX); */
+/*         } */
+
+/*         /\* Remove last visible instance of rocket *\/ */
+/*         usleep(TUNIT); */
+/*         pthread_mutex_lock(&MX); */
+/*         move(myammo->row-1, myammo->col); */
+/*         addch(' '); */
+/*         move(LINES-1, COLS-1); */
+/*         refresh(); */
+/*         pthread_mutex_unlock(&MX); */
+
+/*         /\* Cleanup before exiting thread *\/ */
+/*         myammo->live = 0; */
+/*         myammo->row = 0; */
+/*         myammo->col = 0; */
+/*         pthread_exit(NULL); // TODO difference between this and pthread_cancel in terms of threads array? */
+/* } */
 
 /*
  * TODO add description for this function
@@ -454,7 +619,7 @@ void *saucer_factory(void *arg)
             sleep(1+ rand()%5);
 
             for (i=0; i < MAX_SAUCERS; i++) { // TODO what to do when all MAX_SAUCERS are on screen?
-                if (saucer_array[i].live == 0) {
+                if (saucer_array[i].live == 0 && saucer_array[i].thread == 0) {
                     /* Generates random row value between 0 and LANES-1 */
                     /* Thus generates LANES number of lanes for the saucers to traverse */
                     saucer_array[i].row = rand()%LANES;
@@ -462,6 +627,7 @@ void *saucer_factory(void *arg)
                     /* Generates delay value of 1 plus random # between 0 and 14 - ie random number between 1 and 15 */
                     saucer_array[i].delay = 1 + (rand()%15);
                     saucer_array[i].live = 1;
+                    saucer_array[i].thread = 1;
                     break;
                 }
             }
@@ -470,6 +636,40 @@ void *saucer_factory(void *arg)
         /* Set all saucers to 0 for cleanup */
         for (i=0; i < MAX_SAUCERS; i++)
                 saucer_array[i].live = 0;
+
+        pthread_exit(NULL);
+}
+
+/*
+ * TODO add description for this function
+ * Creates ammo drops when needed
+ */
+void *ammo_drop_factory(void *arg)
+{
+        struct ammo_drop *ammo_drop_array = arg;
+        pthread_t ammo_drop_threads[MAX_AMMO_DROPS];
+        void *animate_ammo_drops();
+        int i;
+        
+        /* Factory loop */
+        while (!QUIT_FLAG) {
+            if (AMMO_FLAG == 1) {
+                for (i = 0; i < MAX_AMMO_DROPS; i++) {
+                    if (ammo_drop_array[i].live == 1 && ammo_drop_array[i].thread == 0) {
+                        // ammo_drop_array[i].new == 0; // TODO no longer needed?
+                        ammo_drop_array[i].thread = 1;
+                        pthread_create(&ammo_drop_threads[i], NULL, animate_ammo_drops, &ammo_drop_array[i]);
+                        break;
+                    }
+                }
+                AMMO_FLAG = 0;
+            }
+            
+        }
+
+        /* Sets all ammo drops to 0 for cleanup */
+        for (i=0; i < MAX_AMMO_DROPS; i++)
+                ammo_drop_array[i].live = 0;
 
         pthread_exit(NULL);
 }
@@ -510,9 +710,9 @@ void *control_input(void *arg)
  * TODO add description for this function
  * Detects rocket strike on saucer
  */
-void strike_check(struct rocket rocket_array[], struct saucer saucer_array[])
+void strike_check(struct rocket rocket_array[], struct saucer saucer_array[], struct ammo_drop ammo_drop_array[])
 {
-        int i, j;
+        int i, j, k;
 
         /* Iterate through all rockets */
         for (i=0; i < MAX_ROCKETS; i++) {
@@ -527,6 +727,21 @@ void strike_check(struct rocket rocket_array[], struct saucer saucer_array[])
                             /* If rocket is within the saucer's position */
                             if ((rocket_array[i].col >= saucer_array[j].col) && 
                                 (rocket_array[i].col <= (saucer_array[j].col + 4))) {
+
+                                /* 1 in 4 chance to spawn an ammo drop */
+                                if ((1 + rand()%1) == 1) { // TODO set back to 4
+                                    for (k = 0; k < MAX_AMMO_DROPS; k++) {
+                                        if (ammo_drop_array[k].live == 0) {
+                                            ammo_drop_array[k].live = 1;
+                                            ammo_drop_array[k].new = 1;
+                                            ammo_drop_array[k].row = rocket_array[i].row + 1; // TODO use +1 here?
+                                            ammo_drop_array[k].col = rocket_array[i].col;
+                                            AMMO_FLAG = 1;
+                                            break;
+                                        }
+                                    }
+                                }
+
                                 /* Set saucer to dead */
                                 saucer_array[j].live = 0;
                                 /* Set rocket to dead */
@@ -534,14 +749,37 @@ void strike_check(struct rocket rocket_array[], struct saucer saucer_array[])
                                 /* Points for killing saucer */
                                 P1SCORE += SAUCER_SCORE;
                                 /* Ammo for killing saucer */
-                                P1AMMO += SAUCER_AMMO;
-                                // TODO set rocket to dead
+                                // TODO remove
+                                //P1AMMO += SAUCER_AMMO;
+
                             }
                         }
                     }
                 }
             }
         }
+}
+
+/*
+ * TODO add description for this function
+ * Detects ammo collection
+ */
+void ammo_collection_check(struct ammo_drop ammo_drop_array[], struct launcher launcher)
+{
+        int i, j;
+
+        for (i=0; i < MAX_AMMO_DROPS; i++) {
+            if (ammo_drop_array[i].live == 1){
+                if (ammo_drop_array[i].row == launcher.row) {
+                    if (ammo_drop_array[i].col == launcher.col) {
+                        ammo_drop_array[i].live = 0;
+                        P1AMMO += 2;
+                        break;
+                    }
+                }
+            }
+        }
+
 }
 
 /*
